49ad3a8806c655d99477fe0498c4fdd54cf0e40c
Justin Reid <jlreid@alumni.unca.edu>
2015-05-25 11:38:24 -0500

    Placed HTML documents in views and switched out of default Bootstrap styling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
 });
 
 module.exports = router;
