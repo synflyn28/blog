a5f42d6e0f05fe2daee75219843aeea751c0cf0e
Justin Reid <jlreid@alumni.unca.edu>
2015-09-21 23:00:32 -0400

    Changed location info

--- public/js/locationMap.js
+++ public/js/locationMap.js
@@ -1,7 +1,7 @@
 //Creates an Google Web map that displays the current location
 function initialize() {
 
-	var myLatlng = new google.maps.LatLng(35.174550, -97.402480);
+	var myLatlng = new google.maps.LatLng(35.524292, -81.142382);
 
 	var mapOptions = {
 		zoom: 15,
@@ -11,7 +11,7 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById('map-canvas'),
 		mapOptions);
 
-	
+
 	var marker = new google.maps.Marker({
 		position: myLatlng,
 		map: map,
@@ -19,8 +19,8 @@ function initialize() {
 	});
 
 	var contentString = "<p>Justin Lynn Reid</p>";
-	contentString += "<p>3625 24th Ave. SE Apt. 5</p>";
-	contentString += "<p>Norman, OK 73071</p>";
+	contentString += "<p>3013 Loop Rd.</p>";
+	contentString += "<p>Lincolnton, NC 28092</p>";
 	contentString += "<p>Phone: (405)-779-6131</p>";
 	contentString += "<p>Email: jlreid@alumni.unca.edu</p>";
 
